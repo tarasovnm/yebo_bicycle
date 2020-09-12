2f692ef5b6e113ad1d522652ae730256da1b7c69
tarasovnm <tarasovnm@gmail.com>
2020-09-12 18:30:48 +0400

    Finished adaptive header stylization

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,10 @@
-//@@include('alert.js')
+var menu = document.querySelector('.menu');
+var menuToggle = document.querySelector('.menu__icon');
+var pageBody = document.querySelector('body');
+
+menuToggle.addEventListener('click', function (evt) {
+  menu.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+  pageBody.classList.toggle('lock');
+});
+
